c53963a0f86cbda61e39e5176f81210ae3901738
hey-prateek <prateektiwari010799@gmail.com>
2025-10-21 01:38:35 +0530

    springboot

---
+++ 3. Spring MVC (Model-View-Controller)/requestLifeCycle/requestLifeCycle/src/main/java/com/example/requestLifeCycle/requestLifeCycle/RequestLifeCycleApplication.java
@@ -0,0 +1,13 @@
+package com.example.requestLifeCycle.requestLifeCycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestLifeCycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestLifeCycleApplication.class, args);
+	}
+
+}
---
+++ 3. Spring MVC (Model-View-Controller)/requestLifeCycle/requestLifeCycle/src/test/java/com/example/requestLifeCycle/requestLifeCycle/RequestLifeCycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.requestLifeCycle.requestLifeCycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestLifeCycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
